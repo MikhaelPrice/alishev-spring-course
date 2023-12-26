e70d047c380e83d0e5cf53638053c249feb1d0db
MichaelPrice <mikhail.a.prus@gmail.com>
2023-12-26 20:02:48 +0300

    Lesson 12. Pre, Post Construct, Scope Annotations

--- src/main/java/spring/course/ClassicalMusic.java
+++ src/main/java/spring/course/ClassicalMusic.java
@@ -1,18 +1,25 @@
 package spring.course;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
+@Scope("prototype")
 public class ClassicalMusic {
 
     public static ClassicalMusic getClassicalMusic(){
         return new ClassicalMusic();
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("Initialization...");
     }
 
+    @PreDestroy
     public void destroy() {
         System.out.println("Destroying...");
     }
